1b40d453e2ba0c6d80a28bea3f0c2118425f41e0
FelipeThums <lulafelipe7@gmail.com>
2025-03-27 01:30:01 -0300

    Adicionando Lista

--- app/src/main/java/com/example/cardstackview/teste.java
+++ app/src/main/java/com/example/cardstackview/teste.java
@@ -7,8 +7,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class teste extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    List<Lista> listaList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,33 @@ public class teste extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        listaList = new ArrayList<>();
+
+        listaList.add(
+                new Lista("Empresa1", R.drawable.logo)
+        );
+        listaList.add(
+                new Lista("Empresa2", R.drawable.logo)
+        );
+        listaList.add(
+                new Lista("Empresa3", R.drawable.logo)
+        );
+        listaList.add(
+                new Lista("Empresa4", R.drawable.logo)
+        );
+        listaList.add(
+                new Lista("Empresa5", R.drawable.logo)
+        );
+
+        recyclerView = findViewById(R.id.idRecLista);
+
+        Adapter adapter = new Adapter(this, listaList);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        recyclerView.setHasFixedSize(true); // Corrigindo o método
+        recyclerView.setAdapter(adapter);
+
+
     }
 }
