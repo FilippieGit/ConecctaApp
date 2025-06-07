2fd398b0cc7ba7f4ac6ba19deaa961f1534da813
Mogitures <davibeserker@outlook.com.br>
2025-06-06 21:25:41 -0300

    NavBarEdit 2.0

--- app/src/main/java/com/example/cardstackview/TelaEmpresaActivity.java
+++ app/src/main/java/com/example/cardstackview/TelaEmpresaActivity.java
@@ -42,9 +42,11 @@ public class TelaEmpresaActivity extends AppCompatActivity {
             if (isEmpresa) {
                 // Empresa: esconde itens que só fazem sentido para candidatos
                 navigationView.getMenu().findItem(R.id.idCriarVagasItemMenu).setVisible(false);
+                navigationView.getMenu().findItem(R.id.idLoginItemMenu).setVisible(false);
             } else {
                 // Candidato: esconde itens que só fazem sentido para empresas
                 navigationView.getMenu().findItem(R.id.idVagasItemMenu).setVisible(false);
+                navigationView.getMenu().findItem(R.id.idLoginItemMenu).setVisible(false);
             }
         }
 
